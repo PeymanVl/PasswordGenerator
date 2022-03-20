f9f5b610413a90af2df2fa60d24ff358202ed71b
Payman Valizadeh <Peyman.Valizadeh@hotmail.com>
2022-03-20 11:31:03 +0330

    Add generate button

--- app.js
+++ app.js
@@ -1,68 +1,72 @@
 
-const passwordInput= document.getElementById('password');
-const inputs=[...document.querySelectorAll('input:not([type="text"])')]
-const lengthInput=document.getElementById('lengthInput');
-const lengthText= document.getElementById('lengthText');
+const passwordInput = document.getElementById('password');
+const inputs = [...document.querySelectorAll('input:not([type="text"])')]
+const lengthInput = document.getElementById('lengthInput');
+// const lengthText= document.getElementById('lengthText');
 const copyButton = document.querySelector('.copy');
+const generatorButton = document.getElementById('generator');
 
-const numbers=[2,3,4,5,6,7,8,9];
-const symbols=['@','#','$','%'];
-const similarLowercaseLetters =['i','l','o'];
-const similarUppercaseLetters =['L','O'];
-const similarNumbers=[0,1];
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9];
+const symbols = ['@', '#', '$', '%'];
+const similarLowercaseLetters = ['i', 'l', 'o'];
+const similarUppercaseLetters = ['L', 'O'];
+const similarNumbers = [0, 1];
 
-const characterSkipCodes =[8,11,14];
-const characterCodes =Array.from(Array(26)).map((_,i)=>i+97);
-const lowercaseLetters = characterCodes.map(code=>String.fromCharCode(code));
-const uppercaseLetters = lowercaseLetters.map(letter=>letter.toUpperCase());
+const characterSkipCodes = [8, 11, 14];
+const characterCodes = Array.from(Array(26)).map((_, i) => i + 97);
+const lowercaseLetters = characterCodes.map(code => String.fromCharCode(code));
+const uppercaseLetters = lowercaseLetters.map(letter => letter.toUpperCase());
 
-copyButton.addEventListener('click',()=>{
+generatorButton.addEventListener('click', () => {
+    updatePassword();
+});
+copyButton.addEventListener('click', () => {
     navigator.clipboard.writeText(passwordInput.value);
-    copyButton.innerText="Copied";
+    copyButton.innerText = "Copied";
     copyButton.classList.add('copied');
     setTimeout(() => {
-        copyButton.innerText="Copy";
+        copyButton.innerText = "Copy";
         copyButton.classList.remove('copied');
     }, 3000);
 })
 
 
 
-const updatePassword = ()=>{
-    const length=lengthInput.value;
-    const checkboxValues=inputs.slice(1).map(input=>input.checked);
-    const password = generatePassword(length,...checkboxValues);
-    passwordInput.value=password;
-    lengthText.innerHtml=`${length}&nbsp;`;
+const updatePassword = () => {
+    const length = lengthInput.value;
+    const checkboxValues = inputs.slice(1).map(input => input.checked);
+    const password = generatePassword(length, ...checkboxValues);
+    passwordInput.value = password;
+    // lengthText.innerHtml=`${length}&nbsp;`;
 }
 
 inputs.forEach(element => {
-    element.addEventListener('change',updatePassword);
+    element.addEventListener('change', updatePassword);
 });
-const generatePassword =(length,hasSymbols,hasNumbers,hasLowercase,hasUppercase,hasSimilars)=>{
+const generatePassword = (length, hasSymbols, hasNumbers, hasLowercase, hasUppercase, hasSimilars) => {
     let availableCharacter = [
-        ...(hasSymbols?symbols:[]),
-        ...(hasNumbers?numbers:[]),
-        ...(hasLowercase?lowercaseLetters:[]),
-        ...(hasUppercase?uppercaseLetters:[]),
+        ...(hasSymbols ? symbols : []),
+        ...(hasNumbers ? numbers : []),
+        ...(hasLowercase ? lowercaseLetters : []),
+        ...(hasUppercase ? uppercaseLetters : []),
     ];
-    if(hasSimilars){
-        if(hasLowercase){
-            availableCharacter=[...availableCharacter,...similarLowercaseLetters];
+    if (hasSimilars) {
+        if (hasLowercase) {
+            availableCharacter = [...availableCharacter, ...similarLowercaseLetters];
         }
-        if(hasUppercase){
-            availableCharacter=[...availableCharacter,...similarUppercaseLetters];
+        if (hasUppercase) {
+            availableCharacter = [...availableCharacter, ...similarUppercaseLetters];
         }
-        
-        if(hasNumbers){
-            availableCharacter=[...availableCharacter,...similarNumbers];
+
+        if (hasNumbers) {
+            availableCharacter = [...availableCharacter, ...similarNumbers];
         }
     }
-    let password ="";
-    if(availableCharacter.length===0)return "";
-    for (let i=0; i<length;i++){
-        const randomIndex=Math.floor(Math.random()* availableCharacter.length);
-        password+= availableCharacter[randomIndex]
+    let password = "";
+    if (availableCharacter.length === 0) return "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * availableCharacter.length);
+        password += availableCharacter[randomIndex]
     }
     return password;
 }
